a21b80c4b39730a31fa7bc32e402ad62198c692d
Keerthiraajan <keerthiraajanbtechcse@gmail.com>
2025-06-05 19:23:28 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,20 @@
+public class pattern {
+
+    public static void main(String[] args) {
+        
+        int n = 7;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (j == 0 || i == j || i == n - 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
+            System.out.println();
+        }
+    }
+}
